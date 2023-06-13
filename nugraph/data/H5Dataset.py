5163a666f44a5edb264fd5845c64152543b538bd
v hewes <vhewes@fnal.gov>
2023-06-13 14:44:52 -0500

    transform data for simpler ground truth

--- nugraph/data/H5Dataset.py
+++ nugraph/data/H5Dataset.py
@@ -3,6 +3,7 @@ from typing import Callable, Optional
 import h5py
 from pynuml import io
 
+import torch
 from torch_geometric.data import Dataset
 
 class H5Dataset(Dataset):
@@ -18,4 +19,22 @@ class H5Dataset(Dataset):
         return len(self._samples)
 
     def get(self, idx: int) -> 'pyg.data.HeteroData':
-        return self._interface.load_heterodata(self._samples[idx])
+        data = self._interface.load_heterodata(self._samples[idx])
+        for p in data.collect('x'):
+            y_filter = data[p].y_f
+
+            y_semantic = torch.empty(y_filter.size()).long()
+            y_semantic[y_filter] = data[p].y_s
+            y_semantic[~y_filter] = -1
+            data[p].y_semantic = y_semantic
+
+            y_instance = torch.empty(y_filter.size()).long()
+            y_instance[y_filter] = data[p].y_i
+            y_instance[~y_filter] = -1
+            data[p].y_instance = y_instance
+
+            del data[p].y_f
+            del data[p].y_s
+            del data[p].y_i
+
+        return data
