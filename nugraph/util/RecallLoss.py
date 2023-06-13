e5f92ed909e8bd18592dc38a312cd92b2ca0a5c6
v hewes <vhewes@fnal.gov>
2023-06-13 16:28:16 -0500

    add ignore_index hook to recall loss

--- nugraph/util/RecallLoss.py
+++ nugraph/util/RecallLoss.py
@@ -5,12 +5,18 @@ import torch.nn.functional as F
 from torchmetrics.functional.classification import multiclass_recall
 
 class RecallLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, ignore_index: int = None):
         super().__init__()
+        self.ignore_index = ignore_index
 
     def forward(self, input, target):
-        weight = 1 - multiclass_recall(input, target, num_classes=input.size(1), average='none')
-        CE = F.cross_entropy(input, target, reduction='none')
+        weight = 1 - multiclass_recall(input, target,
+                                       num_classes=input.size(1),
+                                       average='none',
+                                       ignore_index=self.ignore_index)
+        CE = F.cross_entropy(input, target,
+                             reduction='none',
+                             ignore_index=self.ignore_index)
         loss =  weight[target] * CE
         return loss.mean()
 
