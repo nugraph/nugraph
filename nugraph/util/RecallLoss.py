beb47f336b86e4d51925e1c0bb65afcda1c1d1f8
v hewes <vhewes@fnal.gov>
2023-06-14 11:43:42 -0500

    updates to recall loss function

--- nugraph/util/RecallLoss.py
+++ nugraph/util/RecallLoss.py
@@ -2,21 +2,17 @@
 
 import torch
 import torch.nn.functional as F
-from torchmetrics.functional.classification import multiclass_recall
+from torchmetrics.functional import recall
 
 class RecallLoss(torch.nn.Module):
-    def __init__(self, ignore_index: int = None):
+    def __init__(self, ignore_index: int = -1):
         super().__init__()
         self.ignore_index = ignore_index
 
     def forward(self, input, target):
-        weight = 1 - multiclass_recall(input, target,
-                                       num_classes=input.size(1),
-                                       average='none',
-                                       ignore_index=self.ignore_index)
-        CE = F.cross_entropy(input, target,
-                             reduction='none',
-                             ignore_index=self.ignore_index)
-        loss =  weight[target] * CE
-        return loss.mean()
-
+        weight = 1 - recall(input, target, 'multiclass',
+                            num_classes=input.size(1),
+                            average='none',
+                            ignore_index=self.ignore_index)
+        return F.cross_entropy(input, target, weight=weight,
+                               ignore_index=self.ignore_index)
