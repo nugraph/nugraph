a0277993ff347900f7a27be1deb5934c48304382
v hewes <vhewes@fnal.gov>
2023-06-22 15:10:58 -0500

    update data preparation script

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -2,9 +2,8 @@
 
 import sys
 import argparse
-import nugraph
 
-Data = nugraph.data.H5DataModule
+from nugraph.data import H5DataModule
 
 def configure():
     parser = argparse.ArgumentParser(sys.argv[0])
@@ -12,16 +11,11 @@ def configure():
                         help='Location of input data file')
     parser.add_argument('--batch-size', type=int, default=64,
                         help='Size of each batch of graphs')
-    parser.add_argument('--num-workers', type=int, default=5,
-                        help='Number of worker processes for data loading')
     return parser.parse_args()
 
 def prepare(args):
-    nudata = Data(data_path=args.data_path,
-                  batch_size=args.batch_size,
-                  planes=['u','v','y'],
-                  classes=['MIP','HIP','shower','michel','diffuse'],
-                  prepare=True)
+    H5DataModule.generate_samples(args.data_path)
+    H5DataModule.generate_norm(args.data_path, args.batch_size)
 
 if __name__ == '__main__':
     args = configure()
