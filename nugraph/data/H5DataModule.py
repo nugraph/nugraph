ace3d7c606422a46c151904a819996e338d84901
v hewes <vhewes@fnal.gov>
2023-06-22 14:19:21 -0500

    update handling of metadata in H5DataModule

--- nugraph/data/H5DataModule.py
+++ nugraph/data/H5DataModule.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import warnings
 
 import h5py
+import tqdm
 
 from torch import tensor, cat
 from torch.utils.data import random_split
@@ -27,59 +28,45 @@ class H5DataModule(LightningDataModule):
         self.filename = data_path
         self.batch_size = batch_size
 
-        if prepare:
-            self.generate_samples()
-
         with h5py.File(self.filename) as f:
+
+            # load metadata
             try:
                 self.planes = f['planes'].asstr()[()].tolist()
-                self.classes = f['classes'].asstr()[()].tolist()
+                self.semantic_classes = f['semantic_classes'].asstr()[()].tolist()
+                self.event_classes = f['event_classes'].asstr()[()].tolist()
+            except:
+                print('Metadata not found in file! "planes", "semantic_classes" and "event classes" are required.')
+                exit()
+
+            # load sample splits
+            try:
                 train_samples = f['samples/train'].asstr()[()]
                 val_samples = f['samples/validation'].asstr()[()]
                 test_samples = f['samples/validation'].asstr()[()]
             except:
-                print('samples not found in file! pass "prepare=True" to generate them')
-                exit
+                print('Sample splits not found in file! Call "generate_samples" to create them.')
+                exit()
 
-        transform = Compose((PositionFeatures(self.planes), self.load_norm()))
+            # load feature normalisations
+            try:
+                norm = {}
+                for p in self.planes:
+                    norm[p] = tensor(f[f'norm/{p}'][()])
+            except:
+                print('Feature normalisations not found in file! Call "generate_norm" to create them.')
+                exit()
+
+        transform = Compose((PositionFeatures(self.planes),
+                             FeatureNorm(self.planes, norm)))
 
         self.train_dataset = H5Dataset(self.filename, train_samples, transform)
         self.val_dataset = H5Dataset(self.filename, val_samples, transform)
         self.test_dataset = H5Dataset(self.filename, test_samples, transform)
 
-    def generate_norm(self):
-        with h5py.File(self.filename, 'r+') as f:
-
-            metrics = None
-            samples = list(f['dataset'].keys())
-            loader = DataLoader(H5Dataset(self.filename, samples=samples),
-                                batch_size=self.batch_size)
-
-            print('  generating feature norm...')
-            metrics = None
-            for batch in tqdm.tqdm(loader):
-                for p in self.planes:
-                    x = cat([batch[p].pos, batch[p].x], dim=-1)
-                    if not metrics:
-                        num_feats = x.shape[-1]
-                        metrics = { p: FeatureNormMetric(num_feats) for p in self.planes }
-                    metrics[p].update(x)
-            for p in self.planes:
-                f[f'norm/{p}'] = metrics[p].compute()
-
-    def load_norm(self):
-        norm = {}
-        try:
-            with h5py.File(self.filename) as f:
-                for p in self.planes:
-                    norm[p] = tensor(f[f'norm/{p}'][()])
-        except:
-            print('feature normalisations not found in file! run generate_norm() to generate them.')
-            exit
-        return FeatureNorm(self.planes, norm)
-
-    def generate_samples(self):
-        with h5py.File(self.filename, 'r+') as f:
+    @staticmethod
+    def generate_samples(data_path: str):
+        with h5py.File(data_path, 'r+') as f:
             samples = list(f['dataset'].keys())
             split = int(0.05 * len(samples))
             splits = [ len(samples)-(2*split), split, split ]
@@ -93,6 +80,32 @@ class H5DataModule(LightningDataModule):
             f['samples/train'] = list(train)
             f['samples/validation'] = list(val)
             f['samples/test'] = list(test)
+
+    @staticmethod
+    def generate_norm(data_path: str, batch_size: int):
+        with h5py.File(data_path, 'r+') as f:
+            # load plane metadata
+            try:
+                planes = f['planes'].asstr()[()].tolist()
+            except:
+                print('Metadata not found in file! "planes" is required.')
+                exit()
+
+            loader = DataLoader(H5Dataset(data_path,
+                                          list(f['dataset'].keys()),
+                                          PositionFeatures(planes)),
+                                batch_size=batch_size)
+
+            print('  generating feature norm...')
+            metrics = None
+            for batch in tqdm.tqdm(loader):
+                for p in planes:
+                    if not metrics:
+                        num_feats = batch[p].x.shape[-1]
+                        metrics = { p: FeatureNormMetric(num_feats) for p in planes }
+                    metrics[p].update(batch[p].x)
+            for p in planes:
+                f[f'norm/{p}'] = metrics[p].compute()
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset,
