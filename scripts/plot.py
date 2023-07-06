aa32ceed5d97a6e576adb8b837283fb6f9856568
v hewes <vhewes@fnal.gov>
2023-07-06 15:32:17 -0400

    update plot script
    update plotting script to reflect recent changes in pynuml plotting utility

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,7 +3,6 @@
 from nugraph.util import set_device
 set_device()
 
-import sys
 import os
 import argparse
 import pytorch_lightning as pl
@@ -15,7 +14,7 @@ Data = ng.data.H5DataModule
 Model = ng.models.NuGraph2
 
 def configure():
-    parser = argparse.ArgumentParser(sys.argv[0])
+    parser = argparse.ArgumentParser()
     parser.add_argument('--semantic', action='store_true', default=False,
                         help='Plot semantic labels')
     parser.add_argument('--instance', action='store_true', default=False,
@@ -26,10 +25,27 @@ def configure():
                         help='Checkpoint file to resume training from')
     parser.add_argument('--outdir', type=str, required=True,
                         help='Output directory to write plots to')
+    parser.add_argument('--write-html', action='store_true', default=False,
+                        help='Write HTML files')
+    parser.add_argument('--write-png', action='store_true', default=False,
+                        help='Write PNG files')
+    parser.add_argument('--write-pdf', action='store_true', default=False,
+                        help='Write PDF files')
     parser.add_argument('--limit-predict-batches', type=int, default=10,
                         help='Number of batches to plot')
     parser = Data.add_data_args(parser)
     return parser.parse_args()
+
+def save(plot: pynuml.plot.GraphPlot, data: 'pyg.data.HeteroData',
+         outdir: str, target: str, how: str, filter: str,
+         html: bool, png: bool, pdf: bool):
+    md = data['metadata']
+    tag = f'r{md.run.item()}_sr{md.subrun.item()}_evt{md.event.item()}'
+    name = f'{outdir}/{tag}_{target}_{how}'
+    fig = plot.plot(data, target=target, how=how, filter=filter)
+    if html: fig.write_html(f'{name}.html')
+    if png:  fig.write_image(f'{name}.png')
+    if pdf:  fig.write_image(f'{name}.pdf')
 
 def plot(args):
 
@@ -52,37 +68,35 @@ def plot(args):
             if args.limit_predict_batches is not None and i >= args.limit_predict_batches:
                 break
             for data in batch.to_data_list():
-                md = data['metadata']
-                name = f'r{md.run.item()}_sr{md.subrun.item()}_evt{md.event.item()}'
                 if args.semantic:
-                    plot.plot(data, name=f'{args.outdir}/{name}_semantic_true',
-                              target='semantic', how='true', filter='true')
+                    save(plot, data, args.outdir, 'semantic', 'true', 'true',
+                         args.write_html, args.write_png, args.write_pdf)
                 if args.instance:
-                    plot.plot(data, name=f'{args.outdir}/{name}_instance_true',
-                              target='instance', how='true', filter='true')
+                    save(plot, data, args.outdir, 'instance', 'true', 'true',
+                         args.write_html, args.write_png, args.write_pdf)
                 if args.filter:
-                    plot.plot(data, name=f'{args.outdir}/{name}_filter_true',
-                              target='filter', how='true')
+                    save(plot, data, args.outdir, 'filter', 'true', 'none',
+                         args.write_html, args.write_png, args.write_pdf)
     else: 
         for batch in trainer.predict(model, nudata.test_dataloader()):
             for data in batch:
                 md = data['metadata']
                 name = f'r{md.run.item()}_sr{md.subrun.item()}_evt{md.event.item()}'
                 if args.semantic:
-                    plot.plot(data, name=f'{args.outdir}/{name}_semantic_true',
-                              target='semantic', how='true', filter='true')
-                    plot.plot(data, name=f'{args.outdir}/{name}_semantic_pred',
-                              target='semantic', how='pred', filter='true')
+                    save(plot, data, args.outdir, 'semantic', 'true', 'true',
+                         args.write_html, args.write_png, args.write_pdf)
+                    save(plot, data, args.outdir, 'semantic', 'pred', 'true',
+                         args.write_html, args.write_png, args.write_pdf)
                 if args.instance:
-                    plot.plot(data, name=f'{args.outdir}/{name}_instance_true',
-                              target='instance', how='true', filter='true')
-                    plot.plot(data, name=f'{args.outdir}/{name}_instance_pred',
-                              target='instance', how='pred', filter='true')
+                    save(plot, data, args.outdir, 'instance', 'true', 'true',
+                         args.write_html, args.write_png, args.write_pdf)
+                    save(plot, data, args.outdir, 'instance', 'pred', 'true',
+                         args.write_html, args.write_png, args.write_pdf)
                 if args.filter:
-                    plot.plot(data, name=f'{args.outdir}/{name}_filter_true',
-                              target='filter', how='true')
-                    plot.plot(data, name=f'{args.outdir}/{name}_filter_pred',
-                              target='filter', how='pred')
+                    save(plot, data, args.outdir, 'filter', 'true', 'none',
+                         args.write_html, args.write_png, args.write_pdf)
+                    save(plot, data, args.outdir, 'filter', 'pred', 'none',
+                         args.write_html, args.write_png, args.write_pdf)
 
 if __name__ == '__main__':
     args = configure()
