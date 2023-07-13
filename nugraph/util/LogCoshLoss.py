9845059c59bcc81efc1a2f77d3a63a0812d1a0be
v hewes <vhewes@fnal.gov>
2023-07-13 18:32:54 -0500

    added LogCosh loss module

---
+++ nugraph/util/LogCoshLoss.py
@@ -0,0 +1,11 @@
+import torch
+from torch import Tensor
+import torch.nn.functional as F
+
+class LogCoshLoss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        x = input - target
+        return (x + F.softplus(-2. * x) - torch.log(2.0)).mean()
