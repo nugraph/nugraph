3202307601cd2e1e379ad52fa27e9d8d3fbc6114
v hewes <vhewes@fnal.gov>
2023-08-01 14:41:52 -0400

    add vertex feature argument to train script

--- scripts/train.py
+++ scripts/train.py
@@ -42,6 +42,7 @@ def train(args):
                       node_features=args.node_feats,
                       edge_features=args.edge_feats,
                       sp_features=args.sp_feats,
+                      vertex_features=args.vertex_feats,
                       planes=nudata.planes,
                       semantic_classes=nudata.semantic_classes,
                       event_classes=nudata.event_classes,
