df760dd165a0b98e6dbeb27574a27662b02aab1d
v hewes <vhewes@fnal.gov>
2023-08-24 12:13:15 -0400

    update and simplify testing script

--- scripts/test.py
+++ scripts/test.py
@@ -4,12 +4,10 @@ import sys
 import os
 import time
 import argparse
-import torch
 import pytorch_lightning as pl
 import nugraph as ng
-import h5py
 import pynuml
-import numpy as np
+import tqdm
 
 Data = ng.data.H5DataModule
 Model = ng.models.NuGraph2
@@ -18,8 +16,6 @@ def configure():
     parser = argparse.ArgumentParser(sys.argv[0])
     parser.add_argument('--checkpoint', type=str, required=True,
                         help='Checkpoint file for trained model')
-    parser.add_argument('--device', type=str, default='gpu',
-                        help='Device to use for Model')
     parser.add_argument('--outfile', type=str, required=True,
                         help='Output file name (full path)')
     parser = Data.add_data_args(parser)
@@ -27,22 +23,19 @@ def configure():
 
 def test(args):
 
-    # Load dataset
     print('data path =',args.data_path)
     nudata = Data(args.data_path, batch_size=args.batch_size)
 
     print('using checkpoint =',args.checkpoint)
-    model = Model.load_from_checkpoint(args.checkpoint, map_location=torch.device(args.device))
+    model = Model.load_from_checkpoint(args.checkpoint, map_location='cpu')
 
     print('output file =',args.outfile)
     if os.path.isfile(args.outfile):
-        print('file exists. Exiting.')
-        exit(1)
+        raise Exception(f'file {args.outfile} already exists!')
 
     accelerator, devices = ng.util.configure_device()
     trainer = pl.Trainer(accelerator=accelerator, devices=devices,
-                         #limit_predict_batches=2,
-                         logger=None)
+                         logger=False)
     plot = pynuml.plot.GraphPlot(planes=nudata.planes,
                                  classes=nudata.semantic_classes)
 
@@ -53,25 +46,10 @@ def test(args):
     ngraphs = len(nudata.test_dataset)
     print(f'inference for {ngraphs} events is {itime} s (that\'s {itime/ngraphs} s/graph')
 
-    for ib,batch in enumerate(out):
-        if ib%10==0: print(ib,'/',len(out))
-        md = batch['metadata']
-        df = plot.to_dataframe(batch)
-        r = np.zeros((len(df),), dtype=int)
-        s = np.zeros((len(df),), dtype=int)
-        e = np.zeros((len(df),), dtype=int)
-        offset = 0
-        for p in nudata.planes:
-            for ie in range(0,len(md['run'])):
-                r[offset+batch[p]['ptr'][ie]:offset+batch[p]['ptr'][ie+1]] = md['run'][ie]
-                s[offset+batch[p]['ptr'][ie]:offset+batch[p]['ptr'][ie+1]] = md['subrun'][ie]
-                e[offset+batch[p]['ptr'][ie]:offset+batch[p]['ptr'][ie+1]] = md['event'][ie]
-            offset = offset + len(batch[p]['id'])
-        df['run'] = r
-        df['subrun'] = s
-        df['event'] = e
-        df.to_hdf(args.outfile, 'hits', append=True, format='table') # requires adding pytables to conda env
-
+    for ib, batch in enumerate(tqdm.tqdm(out)):
+        for data in batch.to_data_list():
+            df = plot.to_dataframe(data)
+            df.to_hdf(args.outfile, 'hits', append=True, format='table')
 
 if __name__ == '__main__':
     args = configure()
