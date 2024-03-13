e0aab0c58695f22c34e18316cabb5138c2ea4b9b
v hewes <vhewes@fnal.gov>
2024-03-13 16:18:57 -0400

    updates to interaction network

--- nugraph/models/nugraph3/interaction.py
+++ nugraph/models/nugraph3/interaction.py
@@ -1,53 +1,46 @@
+"""Convolution block for interaction hierarchy level"""
 import torch
 import torch.nn as nn
 from torch.utils.checkpoint import checkpoint
 
-# from torch_geometric.nn import MessagePassing, SimpleConv, MLP
-from torch_geometric.nn.aggr import MeanAggregation
+from torch_geometric.nn.aggr import AttentionalAggregation
 
 T = torch.Tensor
 TD = dict[str, T]
 
-# class InteractionUp(MessagePassing):
-#     def __init__(self,
-#                  planar_features: int,
-#                  planes: list[str],
-#                  aggr: str = 'mean') -> None:
-#         super().__init__(node_dim=0, aggr=aggr)
-
-#         self.edge_net = nn.Sequential(
-#             nn.Linear(planar_features, 1),
-#             nn.Sigmoid(),
-#         )
-
-#     def forward(self, x: T, edge_index: T) -> T:
-#         return self.propagate(x=x, edge_index=edge_index)
-
-#     def message(self, x_j: T) -> T:
-#         return self.edge_net(x_j).detach() * x_j
-
-#     def update(self, aggr_out: T) -> T:
-#         print(aggr_out.shape)
-#         return aggr_out
-
 class InteractionNet(nn.Module):
+    """
+    This convolution block projects graph node features from each plane up to
+    a global embedding representing the interaction as a whole.
+
+    Args:
+        planar_features: Number of features in the planar embedding
+        interaction_features: Number of features in the interaction embedding
+        planes: List containing the names of all planes
+    """
     def __init__(self,
                  planar_features: int,
                  interaction_features: int,
                  planes: list[str]) -> None:
+        """InteractionNet initializer"""
         super().__init__()
 
-        self.att = nn.ModuleDict()
+        self.aggr = nn.ModuleDict()
         for p in planes:
-            self.att[p] = nn.Sequential(
-                nn.Linear(planar_features, 1),
+            self.aggr[p] = AttentionalAggregation(nn.Sequential(
+                nn.Linear(planar_features, planar_features),
                 nn.Sigmoid(),
-            )
-        self.aggr = MeanAggregation()
+            ))
         
         self.net = nn.Linear(len(planes) * planar_features, interaction_features)
 
     def forward(self, x: TD, index: TD) -> T:
-        e = [ self.aggr(net(x[p])*x[p], index=index[p]) for p, net in self.att.items() ]
-        e = torch.cat(e, dim=1)
-        return self.net(e)
+        """Forward pass for InteractionNet 
+        
+        Args:
+            x: Dictionary containing node feature tensors for each plane
+            index: Dictionary containing node index mapping for each plane
+        """
+        x = [ self.aggr(x[p], index[p]) for p, net in self.aggr.items() ]
+        for p in self.aggr: print(p, x[p].shape)
+        return self.net(torch.cat(x, dim=1))
