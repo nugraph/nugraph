0a1f8e7618abecfa81e4d72217b009876a7271ee
v hewes <v@waveland.io>
2024-06-19 18:47:16 -0400

    materialize instances

--- nugraph/models/nugraph3/decoders/instance.py
+++ nugraph/models/nugraph3/decoders/instance.py
@@ -3,6 +3,7 @@ from typing import Any
 import pathlib
 import torch
 from torch import nn
+from torch_geometric.data import Batch
 from pytorch_lightning.loggers import TensorBoardLogger
 import pandas as pd
 from sklearn.decomposition import PCA
@@ -87,6 +88,35 @@ class InstanceDecoder(DecoderBase):
         x_filter = torch.cat([batch[p]["of"] for p in self.planes], dim=0)
         y = torch.cat([batch[p]["y_instance"] for p in self.planes], dim=0)
         return (x_coords, x_filter), y
+
+    def materialize(self, data: Batch) -> None:
+        """
+        Materialize object condensation embedding into instances
+
+        Args:
+            data: Heterodata graph object
+        """
+        of = torch.cat([data[p].of for p in self.planes], dim=0)
+        ox = torch.cat([data[p].ox for p in self.planes], dim=0)
+        centers = (of > 0.1).nonzero().squeeze(0)
+        distances = []
+        for center in centers:
+            center_coords = ox[center]
+            dist = 1 - (ox - center_coords).square().sum(dim=1).sqrt()
+            dist[(dist<0)] = 0
+            distances.append(dist)
+        if distances:
+            distances = torch.stack(distances, dim=1)
+            total = distances.sum(dim=1)
+            mask = (total > 0)
+            xi = distances.argmax(dim=1)
+            xi[~mask] = -1
+            ts = xi.split([data[p].num_nodes for p in self.planes])
+            for p, t in zip(self.planes, ts):
+                data[p].i = t.int()
+        else:
+            for p in self.planes:
+                data[p].i = torch.empty_like(data[p].of).fill_(-1).int()
     
     def metrics(self, x: T, y: T, stage: str) -> dict[str, Any]:
         """
@@ -98,6 +128,24 @@ class InstanceDecoder(DecoderBase):
             stage: Training stage
         """
         return {}
+
+    def finalize(self, data) -> None:
+        """
+        Finalize outputs for NuGraph3 instance decoder
+
+        Args:
+            data: Graph data object
+        """
+        if isinstance(data, Batch):
+            dlist = data.to_data_list()
+            for d in dlist:
+                self.materialize(d)
+            for p in self.planes:
+                data[p].i = torch.cat([d[p].i for d in dlist], dim=0)
+                data._slice_dict[p]["i"] = data._slice_dict[p]["x"]
+                data._inc_dict[p]["i"] = data._inc_dict[p]["x"]
+        else:
+            self.materialize(data)
 
     def draw_event_display(self, data: "pyg.HeteroData"):
         """
