79bc1a201980d7ccfdd086ebed09a1bc9c006fa0
v hewes <v@waveland.io>
2024-08-14 11:43:11 -0400

    update hierarchical edges

--- nugraph/nugraph/util/hierarchical_edges.py
+++ nugraph/nugraph/util/hierarchical_edges.py
@@ -45,15 +45,13 @@ class HierarchicalEdges(BaseTransform):
 
         # add edges to and from event node
         data["evt"].num_nodes = 1
-        for p in self.planes + ["sp"]:
-            lo = torch.arange(data[p].num_nodes, dtype=torch.long)
-            hi = torch.zeros(data[p].num_nodes, dtype=torch.long)
-            data[p, "in", "evt"].edge_index = torch.stack((lo, hi), dim=0)
-            data["evt", "owns", p].edge_index = torch.stack((hi, lo), dim=0)
+        lo = torch.arange(data["hit"].num_nodes, dtype=torch.long)
+        hi = torch.zeros(data["hit"].num_nodes, dtype=torch.long)
+        data["hit", "in", "evt"].edge_index = torch.stack((lo, hi), dim=0)
+        data["evt", "owns", "hit"].edge_index = torch.stack((hi, lo), dim=0)
 
         # add edges from nexus to plane
-        for p in self.planes:
-            lo, hi = data[p, "nexus", "sp"].edge_index
-            data["sp", "nexus", p].edge_index = torch.stack((hi, lo), dim=0)
+        lo, hi = data["hit", "nexus", "sp"].edge_index
+        data["sp", "nexus", "hit"].edge_index = torch.stack((hi, lo), dim=0)
 
         return data
