6c855c0d4d04ec09fb9fae887551f4bd4d7e6e01
v hewes <v@waveland.io>
2024-08-21 21:50:11 -0400

    calculate instance loss individually for each graph in batch

--- nugraph/nugraph/util/ObjCondensationLoss.py
+++ nugraph/nugraph/util/ObjCondensationLoss.py
@@ -10,11 +10,14 @@ class ObjCondensationLoss(torch.nn.Module):
     def background_loss(self, beta: Tensor, y: Tensor) -> Tensor:
         K = y.max() + 1
         n_i = (y == -1)
-        M_ik = torch.zeros((y.size(0), K), device=y.device).long()
-        M_ik[~n_i,:] = torch.nn.functional.one_hot(y[~n_i], num_classes=K)
-        beta_ak = (beta[:,None] * M_ik).max(dim=0).values
         N_b = n_i.sum()
-        L_beta_1 = (1 - beta_ak).sum() / K
+        if K:
+            M_ik = torch.zeros((y.size(0), K), device=y.device).long()
+            M_ik[~n_i,:] = torch.nn.functional.one_hot(y[~n_i], num_classes=K)
+            beta_ak = (beta[:,None] * M_ik).max(dim=0).values
+            L_beta_1 = (1 - beta_ak).sum() / K
+        else:
+            L_beta_1 = 0
         L_beta_2 = (self.S_b / N_b) * (n_i * beta).sum()
         L_beta = torch.sum(L_beta_1 + L_beta_2)
         return L_beta
@@ -23,7 +26,8 @@ class ObjCondensationLoss(torch.nn.Module):
         K = y.max() + 1
         n_i = (y == -1)
         M_ik = torch.zeros((y.size(0), K), device=y.device).long()
-        M_ik[~n_i,:] = torch.nn.functional.one_hot(y[~n_i], num_classes=K)
+        if K:
+            M_ik[~n_i,:] = torch.nn.functional.one_hot(y[~n_i], num_classes=K)
         q_i = beta.atanh().square() + self.q_min
         q_max = (q_i[:,None] * M_ik).max(dim=0)
         q_ak = q_max.values
