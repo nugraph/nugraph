d404c7a94998aefae0df1e19e34b4fe90fe56ca8
v hewes <v@waveland.io>
2024-08-23 21:40:37 -0400

    overhaul potential terms in object condensation loss

--- nugraph/nugraph/util/ObjCondensationLoss.py
+++ nugraph/nugraph/util/ObjCondensationLoss.py
@@ -9,33 +9,13 @@ class ObjCondensationLoss(torch.nn.Module):
         super().__init__()
         self.s_b = s_b
         self.q_min = q_min
-    
-    def potential_loss(self, x: Tensor, beta: Tensor, y: Tensor) -> Tensor:
-        K = y.max() + 1
-        n_i = (y == -1)
-        M_ik = torch.zeros((y.size(0), K), device=y.device).long()
-        M_ik[~n_i,:] = torch.nn.functional.one_hot(y[~n_i], num_classes=K)
-        q_i = beta.atanh().square() + self.q_min
-        q_max = (q_i[:,None] * M_ik).max(dim=0)
-        q_ak = q_max.values
-        idx_ak = q_max.indices
-        x_a = x[idx_ak]
-        x_diff = (x[:,None,:] - x_a[None,:,:]).square().sum(dim=2)
-        x_inv = 1 - x_diff
-        x_inv[(x_inv < 0)] = 0
-        V_k_a = x_diff[None,:] * q_ak
-        V_k_r = x_inv[None, :] * q_ak
-        N=y.size(0)
-        L_v_1=M_ik*(V_k_a)
-        L_v_2=(1-M_ik)*(V_k_r)
-        L_v=((L_v_1+L_v_2).sum(dim=2) * q_i).sum () / N
-        return L_v
 
     def forward(self, data: HeteroData, y: Tensor) -> Tensor:
 
         device = data["hit"].x.device
 
         # hit information
+        n_hit = data["hit"].num_nodes
         x = data["hit"].ox
         f = data["hit"].of
 
@@ -54,16 +34,25 @@ class ObjCondensationLoss(torch.nn.Module):
             raise RuntimeError(("Cannot compute object condensation loss "
                                 "when there is no true background!"))
 
-        # determine which hit is the condensation point
-        # for each true instance, and get their beta values
-        # f_true and their hit indices i_true
-        i, j = e_true
+        # determine which hit is the condensation point for each true instance,
+        # and get beta values (f_centers) and hit indices (centers)
+        e_h, e_p = e_true
         f_centers = torch.zeros(n_true, device=device)
-        f_centers, i_centers = scatter_max(f[i], j, out=f_centers)
-        i_centers = i[i_centers]
+        f_centers, centers = scatter_max(f[e_h], e_p, out=f_centers)
+        centers = e_h[centers]
 
-        # calculate the two background loss terms, b1 and b2
+        # calculate background loss terms
         b1 = 1 - (f_centers.sum() / n_true)
         b2 = (self.s_b / n_bkg) * f[bkg_mask].sum()
 
-        return b1 + b2 + self.potential_loss(x, f, y)
+        # calculate the charge on each hit
+        q = f.atanh().square() + self.q_min
+
+        # calculate attractive and repulsive potentials
+        m_ik = torch.zeros(n_hit, n_true, dtype=torch.bool, device=device)
+        m_ik[e_h, e_p] = True
+        dist = (x[:, None, :] - x[centers][None, :, :]).square().sum(dim=2)
+        v = torch.where(m_ik, dist, (1 - dist).clamp(0))
+        v = ((v * q[centers]).sum(dim=1) * q).sum() / n_hit
+
+        return b1 + b2 + v
