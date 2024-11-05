5c333392f66443c656b67607019b38801a8f4009
v hewes <vhewes@fnal.gov>
2024-11-05 12:43:37 -0500

    add method to save data object to compound dataset

--- pynuml/pynuml/data/nugraph_data.py
+++ pynuml/pynuml/data/nugraph_data.py
@@ -1,5 +1,6 @@
 """NuGraph data object"""
 import h5py
+import numpy as np
 import torch
 from torch_scatter import scatter_min
 from torch_geometric.data import Batch, HeteroData
@@ -125,6 +126,53 @@ class NuGraphData(HeteroData):
         if key == "cluster_true_label":
             return self[N_IT].num_nodes
         return super().__inc__(key, value, *args, **kwargs)
+
+    def save(self, file: h5py.File, name: str) -> None:
+        """Save NuGraph data object to HDF5 file
+
+        Args:
+            file: HDF5 file to save graph to
+            name: Name of dataset to save graph to
+        """
+
+        data = []
+        fields = []
+
+        nodes, edges = self.metadata()
+        stores = [(key, key) for key in nodes] + [(key, "_".join(key)) for key in edges]
+
+        # loop over data tensors
+        for store, prefix in stores:
+
+            # check for syntax errors in data store names
+            if isinstance(store, tuple) and prefix.count("_") != 2:
+                raise ValueError((f"\"{prefix}\" is not a valid edge store name!"
+                                  " Too many underscores."))
+            if isinstance(store, str) and prefix.count("_"):
+                raise ValueError((f"\"{prefix}\" is not a valid node store name!"
+                                  " Underscores are not supported."))
+
+            # add attributes to compound data object
+            for attr in self[store].keys():
+                key = f"{prefix}/{attr}"
+                val = self[store][attr]
+                if np.isscalar(val):
+                    data.append(val)
+                    fields.append((key, type(val)))
+                elif val.nelement() == 0: # save tensor with zero-sized dimension as a scalar 0
+                    # HDF5 compound data type does not allow zero-size dimension
+                    # ValueError: Zero-sized dimension specified (zero-sized dimension specified)
+                    data.append(0)
+                    fields.append((key, val.numpy().dtype))
+                else:
+                    val = val.numpy() # convert a tensor to numpy
+                    data.append(val)
+                    fields.append((key, val.dtype, val.shape))
+
+        # create a scalar dataset of compound data type
+        ctype = np.dtype(fields)
+        ds = file.create_dataset(name, shape=(), dtype=ctype, data=tuple(data))
+        del ctype, fields, data, ds
 
     @classmethod
     def load(cls, dset: h5py.Dataset) -> "NuGraphData":
