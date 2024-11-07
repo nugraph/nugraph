79180c162c51f8521e41fffbcad4275a409c6c4d
v hewes <v@waveland.io>
2024-11-07 13:30:55 -0500

    use HDBSCAN to materialize clusters

--- pynuml/pynuml/data/nugraph_data.py
+++ pynuml/pynuml/data/nugraph_data.py
@@ -1,10 +1,10 @@
 """NuGraph data object"""
 import h5py
 import numpy as np
+from sklearn.cluster import HDBSCAN
 import torch
-from torch_scatter import scatter_min
 from torch_geometric.data import Batch, HeteroData
-from torch_geometric.utils import bipartite_subgraph, degree
+from torch_geometric.utils import unbatch
 
 N_IT = "particle-truth" # true instance node store
 N_IP = "particle" # predicted instance node store
@@ -16,10 +16,10 @@ class NuGraphData(HeteroData):
 
     # pylint: disable=abstract-method
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def true_instance_labels(self, regenerate: bool = False) -> None:
+    def true_instance_labels(self, regenerate: bool = False) -> torch.Tensor:
         """Return true instance labels on hits
 
         Args:
@@ -27,14 +27,13 @@ class NuGraphData(HeteroData):
         """
 
         # materialize clusters if necessary
-        if regenerate or not (hasattr(self["hit"], "cluster_true_label") and \
-            hasattr(self["hit"], "cluster_true_mask")):
+        if regenerate or not (hasattr(self["hit"], "y_instance") and \
+            hasattr(self["hit"], "y_instance_mask")):
             self.materialize_true_particles()
 
         # return labels
-        ret = torch.empty_like(self["hit"].cluster_true_mask, dtype=torch.long).fill_(-1)
-        ret[self["hit"].cluster_true_mask] = self["hit"].cluster_true_label
-        return ret
+        self["hit"].y_instance[~self["hit"].y_instance_mask] = -1
+        return self["hit"].y_instance
 
     def predicted_instance_labels(self, regenerate: bool = False) -> None:
         """Return predicted instance labels on hits
@@ -44,86 +43,79 @@ class NuGraphData(HeteroData):
         """
 
         # materialize clusters if necessary
-        if regenerate or not (hasattr(self["hit"], "cluster_pred_label") and \
-            hasattr(self["hit"], "cluster_pred_mask")):
+        if regenerate or not (hasattr(self["hit"], "x_instance") and \
+            hasattr(self["hit"], "x_instance_mask")):
             self.materialize_predicted_particles()
 
-        # return labels
-        ret = torch.empty_like(self["hit"].cluster_pred_mask, dtype=torch.long).fill_(-1)
-        ret[self["hit"].cluster_pred_mask] = self["hit"].cluster_pred_label
-        return ret
+        # fix background labels and return
+        self["hit"].x_instance[~self["hit"].x_instance_mask] = -1
+        return self["hit"].x_instance
 
     def materialize_true_particles(self) -> None:
         """Materialize true particle clusters"""
 
         # assign particle labels and mask
         i, j = self[E_H_IT].edge_index
-        self["hit"].cluster_true_label = j
-        m = torch.zeros_like(self["hit"].y_semantic, dtype=torch.bool)
-        m[i] = True
-        self["hit"].cluster_true_mask = m
+        self["hit"].y_instance = torch.empty_like(self["hit"].y_semantic).fill_(-1)
+        self["hit"].y_instance[i] = j
+        self["hit"].y_instance_mask = self["hit"].y_instance > -1
 
     def materialize_predicted_particles(self) -> None:
         """Materialize predicted particle nodes"""
 
-        if isinstance(self, Batch):
-            raise RuntimeError("Materializing of clusters must not be performed over batch.")
+        def go(x, f):
+            mask = f > 0.5
+            i = torch.empty_like(mask, dtype=torch.long)
+            i[~mask] = -1
+            i[mask] = torch.tensor(HDBSCAN().fit(x[mask]).labels_)
+            return i
 
-        if not (hasattr(self["hit"], "cluster_pred_label") \
-            and hasattr(self["hit"], "cluster_pred_mask")):
-            raise RuntimeError(("Cannot call materialize_predicted_particles() without running "
-                                "NuGraph3 inference first!"))
+        if not isinstance(self, Batch):
+            i = go(self["hit"].ox, self["hit"].x_filter)
+            self["hit"].x_instance = i
+            self["hit"].x_instance_mask = i > -1
+            return
 
-        # mask to find condensation points
-        fmask = self["hit"].of > 0.1
-        self[N_IP].x = torch.empty(fmask.sum(), 0, device=self.device)
-        self[N_IP].ox = self["hit"].ox[fmask]
-        fidx = fmask.nonzero().squeeze(1)
+        coords = unbatch(self["hit"].ox, self["hit"].batch, batch_size=self.num_graphs)
+        mask = unbatch(self["hit"].x_filter, self["hit"].batch, batch_size=self.num_graphs)
+        labels = [go(x, f) for x, f in zip(coords, mask)]
 
-        e = self[E_H_IP]
-        x_hit = self["hit"].ox
+        out = [NuGraphData(hit={"x_instance": l, "x_instance_mask": l > -1},
+                           particle={"x": torch.empty(l.max()+1)})
+               for l in labels]
+        out = Batch.from_data_list(out)
 
-        # initial particle instance nodes
-        self[N_IP].x = torch.empty(fidx.size(0), 0, device=self.device)
-        self[N_IP].ox = x_hit[fmask]
+        def copy_attr(d, store, attr):
+            # pylint: disable=protected-access
+            setattr(self[store], attr, getattr(d[store], attr))
+            if store in self._slice_dict:
+                self._slice_dict[store][attr] = d._slice_dict[store][attr]
+            else:
+                self._slice_dict[store] = {attr: d._slice_dict[store][attr]}
+            if store in self._inc_dict:
+                self._inc_dict[store][attr] = d._inc_dict[store][attr]
+            else:
+                self._inc_dict[store] = {attr: d._inc_dict[store][attr]}
 
-        # add edges from condensation hits to non-condensation hits
-        dist = (x_hit[~fmask, None, :] - x_hit[None, fmask, :]).square().sum(dim=2)
-        e.edge_index = (dist < 1).nonzero().transpose(0, 1).detach()
-        e.distance = dist[e.edge_index[0], e.edge_index[1]].detach()
-        e.edge_index[0] = torch.nonzero(~fmask).squeeze(1)[e.edge_index[0]]
+        copy_attr(out, "hit", "x_instance")
+        copy_attr(out, "hit", "x_instance_mask")
+        copy_attr(out, "particle", "x")
 
-        # prune particle instances with no hits
-        deg = degree(e.edge_index[1], num_nodes=self[N_IP].num_nodes)
-        dmask = deg >= self.min_degree
-        e.edge_index, e.distance = bipartite_subgraph( # pylint: disable=unbalanced-tuple-unpacking
-            (torch.ones(self["hit"].num_nodes, dtype=torch.bool, device=self.device), dmask),
-            e.edge_index, e.distance, size=(self["hit"].num_nodes, self[N_IP].num_nodes),
-            relabel_nodes=True)
-        self[N_IP].x = self[N_IP].x[dmask]
-        self[N_IP].ox = self[N_IP].ox[dmask]
+    @property
+    def y_i(self) -> torch.Tensor:
+        """True instance labels"""
+        return self.true_instance_labels()
 
-        #  add edges from particle nodes to condensation hits
-        pidx = fidx[dmask]
-        dist = (x_hit[fidx, None, :] - x_hit[None, pidx, :]).square().sum(dim=2)
-        edge_index = (dist < 1).nonzero().transpose(0, 1).detach()
-        if edge_index.size(1):
-            distance = dist[edge_index[0], edge_index[1]].detach()
-            edge_index[0] = fidx[edge_index[0]]
-            e.edge_index = torch.cat((e.edge_index, edge_index), dim=1)
-            e.distance = torch.cat((e.distance, distance), dim=0)
-
-        # collapse cluster edges into node labels
-        _, instances = scatter_min(e.distance, e.edge_index[0], dim_size=self["hit"].num_nodes)
-        m = instances < e.num_edges # nodes without edges are set to e.num_edges
-        self["hit"].cluster_pred_label = instances[m]
-        self["hit"].cluster_pred_mask = m
+    @property
+    def x_i(self) -> torch.Tensor:
+        """Predicted instance labels"""
+        return self.predicted_instance_labels()
 
     def __inc__(self, key: str, value: torch.Tensor, *args, **kwargs) -> int:
         """Increment tensor values"""
-        if key == "cluster_pred_label":
+        if key == "x_instance":
             return self[N_IP].num_nodes
-        if key == "cluster_true_label":
+        if key == "y_instance":
             return self[N_IT].num_nodes
         return super().__inc__(key, value, *args, **kwargs)
 
