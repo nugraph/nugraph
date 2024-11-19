9a61785d33ddc732e6205ead1d71119c11a2ab48
v hewes <v@waveland.io>
2024-11-19 14:33:46 -0500

    disable work-in-progress nugraph unit tests

--- nugraph/tests/test_compile.py
+++ nugraph/tests/test_compile.py
@@ -1,5 +1,5 @@
 import nugraph as ng
 
-def test_compile_nugraph2():
-    """Test compilation of the NuGraph2 model."""
-    ng.models.NuGraph2().to_torchscript()
+# def test_compile_nugraph2():
+#     """Test compilation of the NuGraph2 model."""
+#     ng.models.NuGraph2().to_torchscript()
