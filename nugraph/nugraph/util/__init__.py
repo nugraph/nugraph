102311b2572779256e61f353e3fbb817d9196bdc
v hewes <v@waveland.io>
2025-02-13 18:35:03 -0500

    remove feature norm from nugraph utils

--- nugraph/nugraph/util/__init__.py
+++ nugraph/nugraph/util/__init__.py
@@ -3,7 +3,6 @@ from .RecallLoss import RecallLoss
 from .LogCoshLoss import LogCoshLoss
 from .ObjCondensationLoss import ObjCondensationLoss
 from .position_features import PositionFeatures
-from .feature_norm import FeatureNorm, FeatureNormMetric
 from .hierarchical_edges import HierarchicalEdges
 from .event_labels import EventLabels
 from .scriptutils import setup_env, configure_device
